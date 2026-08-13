eb56620ddc1bf50f918d755f0150d9d2a28db677
John P. Lucas <john.lucas@voyagertechnologies.us>
2026-08-13 14:36:30 -0400

    [#1] Added ci_lab_topicsids.h forwarding stub for bridge include path for generate_configfile_set(); (#1)

---
+++ config/ci_lab_topicids.h
@@ -0,0 +1,7 @@
+/**
+ * @file ci_lab_topicids.h
+ *
+ * Forwarding header placed in config/ so that default_ci_lab_msgid_values.h
+ * can find it regardless of the compilation target's include path.
+ */
+#include "../fsw/inc/ci_lab_topicids.h"
